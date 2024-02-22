598a99fbc8384eabff02665a2a8f7a93eaefe3af
Akshay <akshayagrawal8292@gmail.com>
2024-02-23 00:01:12 +0530

    initial code

---
+++ src/app/app.component.ts
@@ -0,0 +1,43 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-root',
+  templateUrl: './app.component.html',
+  styleUrls: ['./app.component.css']
+})
+export class AppComponent {
+  title = 'angular-semicolon';
+  opened = true;
+
+  toggle(): void {
+    this.opened = !this.opened;
+  }
+
+  menu: any = [
+    {
+      title: 'Home',
+      icon: 'home',
+      link: '/home',
+      color: '#ff7f0e',
+    },
+    {
+      title: 'Statistics',
+      icon: 'bar_chart',
+      color: '#ff7f0e',
+      subMenu: [
+        {
+          title: 'Sales',
+          icon: 'money',
+          link: '/sales',
+          color: '#ff7f0e',
+        },
+        {
+          title: 'Customers',
+          icon: 'people',
+          color: '#ff7f0e',
+          link: '/customers',
+        },
+      ],
+    },
+  ];
+}
---
+++ src/app/app.module.ts
@@ -0,0 +1,46 @@
+import { NgModule } from '@angular/core';
+import { BrowserModule } from '@angular/platform-browser';
+
+import { AppRoutingModule } from './app-routing.module';
+import { AppComponent } from './app.component';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { MatIconModule } from '@angular/material/icon';
+import { MatButtonModule } from '@angular/material/button';
+import { MatSidenavModule } from '@angular/material/sidenav';
+import { MatListModule } from '@angular/material/list';
+import { MatToolbarModule } from '@angular/material/toolbar';
+import { RouterModule, Routes } from '@angular/router';
+import { MatProgressSpinnerModule } from '@angular/material/progress-spinner';
+import { MatMenuModule } from '@angular/material/menu';
+import { MatExpansionModule } from '@angular/material/expansion';
+import { FormsModule } from '@angular/forms';
+import { HeaderComponent } from './components/header/header.component';
+import { MenuComponent } from './components/menu/menu.component';
+import { HomeComponent } from './components/home/home.component';
+
+@NgModule({
+  declarations: [
+    AppComponent,
+    HeaderComponent,
+    MenuComponent,
+    HomeComponent
+  ],
+  imports: [
+    BrowserModule,
+    AppRoutingModule,
+    BrowserAnimationsModule,
+    FormsModule,
+    MatIconModule,
+    MatButtonModule,
+    MatToolbarModule,
+    MatSidenavModule,
+    MatListModule,
+    // RouterModule.forRoot(),
+    MatProgressSpinnerModule,
+    MatExpansionModule,
+    MatMenuModule
+  ],
+  providers: [],
+  bootstrap: [AppComponent]
+})
+export class AppModule { }
